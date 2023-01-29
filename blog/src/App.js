989361c1da8f83aa2ac10ebdc8abb2177f5ed673
EunJu JO <dmswnehf@naver.com>
2023-01-29 21:09:55 +0900

    1/29-2  commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,25 +4,38 @@ import { useState } from 'react';
 
 function App() {
 
-  let [title, b] = useState(['남자 코드 추천', '강남 우동 맛집', '파이썬독학']);
-  let [content, setCon] = useState('2월 17일 발행')
+  let [title, 글제목변경] = useState(['남자 코드 추천', '강남 우동 맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button onClick={()=>{
+        let copy = [...title];
+        copy.sort();
+        글제목변경(copy);
+      }}>가나다순정렬</button>
+
+      <button onClick={()=>{
+        let copy = [...title]; 
+        copy[0] = '여자코트 추천';
+        글제목변경(copy);
+      }}>글 수정</button>
+
       <div className="list">
-        <h4>{ title[0] }</h4>
-        <p>{content}</p>
+        <h4>{ title[0] } <span onClick={ ()=> {따봉변경(따봉+1)} }>좋아요 👍</span> {따봉} </h4>
+        <p>2월 17일 발행</p>
       </div>
       <div className="list">
         <h4>{ title[1] }</h4>
-        <p>{content}</p>
+        <p>2월 17일 발행</p>
       </div>
       <div className="list">
         <h4>{ title[2] }</h4>
-        <p>{content}</p>
+        <p>2월 17일 발행</p>
       </div>
     </div>
   );

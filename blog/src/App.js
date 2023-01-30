9552a29156330b7b02e56eb0cbb8ccef3a6f746c
EunJu JO <dmswnehf@naver.com>
2023-01-31 04:04:30 +0900

    1/30 commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -33,12 +33,26 @@ function App() {
         <h4>{ title[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
+
       <div className="list">
         <h4>{ title[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal></Modal>
+
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
